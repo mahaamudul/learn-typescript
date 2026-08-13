b8aa2993c117cfab151324e8fbde7293fe938852
Mahamudul Hasan Mehedi <mahamudul.hasan.mehedi.01@gmail.com>
2026-08-13 17:32:44 +0600

    object type

--- module 1/test.ts
+++ module 1/test.ts
@@ -1,3 +1,22 @@
 // const myName: string = "John Doe";
 // console.log(myName);
 
+
+
+//object type 
+
+const user: {
+    isMarried: boolean;
+    firstName: string;
+    middleName?: string;// optional property
+    lastName: string;
+    nationality?: "bangladeshi"; // value type || literal type
+} = {
+    firstName:"mahamudul",
+    middleName:"hasan",
+    lastName:"mehedi",
+    isMarried:true,
+    nationality:"bangladeshi"
+}
+
+console.log(user);
