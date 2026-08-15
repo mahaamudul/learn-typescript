841ebaea8423973eb3a39387bceeb3f84ad4b8ea
Mahamudul Hasan Mehedi <mahamudul.hasan.mehedi.01@gmail.com>
2026-08-16 00:30:21 +0600

    get single product

--- module 6/src/controllers/product.controller.ts
+++ module 6/src/controllers/product.controller.ts
@@ -1,16 +1,32 @@
+
 import type { IncomingMessage, ServerResponse } from "node:http";
 import { readProduct } from "../service/product.service";
+import type { IProduct } from "../types/products.types";
 
 export const productController=(req:IncomingMessage,res:ServerResponse)=>{
     const url=req.url;
     const method=req.method;
+    const urlParts=url?.split('/')
+    const id=urlParts && urlParts[1]==="products"?Number(urlParts[2]): null
 
     const products=readProduct()
+    // console.log(products);
 
 
     if (url==="/products" && method==="GET"){
         console.log("this is product");
         res.writeHead(200,{"content-type":"application/json"});
          res.end(JSON.stringify({message:"this is products route ", data:products}))
+    }
+    else if (method==="GET" && id !== null){
+    const products=readProduct()
+
+        const product = products.find((p:IProduct) => p.id === id);
+        console.log(product);
+
+        res.writeHead(200,{"content-type":"application/json"});
+         res.end(JSON.stringify({message:"this is single product route ", data:product}))
+
+
     }
 }
