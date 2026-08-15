36aade91b5ee877d2951aaa0d418eb1db3f8a31c
Mahamudul Hasan Mehedi <mahamudul.hasan.mehedi.01@gmail.com>
2026-08-15 16:17:31 +0600

    create-route

--- module 6/src/server.ts
+++ module 6/src/server.ts
@@ -1,7 +1,26 @@
 import { createServer, IncomingMessage, Server } from "node:http";
 
 const server:Server=createServer((req:IncomingMessage,res)=>{
-    console.log(req);
+    console.log(req.url);
+    console.log(req.method);
+
+    const url=req.url;
+    const method=req.method;
+
+
+    if (url==="/" && method==="GET"){
+        console.log("this is root");
+        res.writeHead(200,{"content-type":"application/json"});
+         res.end(JSON.stringify({message:"this is root dir "}))
+    }
+    else if (url?.startsWith("/products")){
+        res.writeHead(200,{"content-type":"application/json"});
+         res.end(JSON.stringify({message:"this is product route "}))
+    }
+    else {
+        res.writeHead(404,{"content-type":"application/json"});
+        res.end(JSON.stringify({message:"page not found "}))
+    }
 })
 
 server.listen(5000,()=>{
