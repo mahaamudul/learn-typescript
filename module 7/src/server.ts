d04badcc8a173a560b0dc24f3ca61e3be2d46c09
Mahamudul Hasan Mehedi <mahamudul.hasan.mehedi.01@gmail.com>
2026-08-17 15:16:06 +0600

    delete user

--- module 7/src/server.ts
+++ module 7/src/server.ts
@@ -139,7 +139,15 @@ app.put('/api/users/:id', async (req: Request, res: Response) => {
     try {
         const result = await pool.query(`
         
-        UPDATE users SET name=$1,password=$2, age=$3, is_active=$4 WHERE id=$5
+        UPDATE users
+        SET 
+
+        name=COALESCE($1, name),
+        password=COALESCE($2, password), 
+        age=COALESCE($3, age), 
+        is_active=COALESCE($4 ,is_active)
+
+        WHERE id=$5
         RETURNING *
         
         `, [name, password, age, is_active, id])
@@ -157,6 +165,49 @@ app.put('/api/users/:id', async (req: Request, res: Response) => {
             message: "User Updated",
             success: true,
             data: result.rows[0]
+        })
+    }
+    catch (err: any) {
+
+        res.status(500).json({
+            message: err.message,
+            success: false,
+            "error": err
+        })
+
+    }
+
+
+
+
+
+})
+
+// delete user
+app.delete('/api/users/:id', async (req: Request, res: Response) => {
+    const { id } = req.params;
+    
+
+    try {
+        const result = await pool.query(`
+        DELETE FROM users WHERE id=$1
+        
+        RETURNING *
+        
+        `, [id])
+
+        if (result.rows.length === 0) {
+            res.status(404).json({
+                message: "User Not found !",
+                success: false,
+                data: {}
+            })
+
+        }
+
+        res.status(200).json({
+            message: "User Deleted",
+            success: true,
         })
     }
     catch (err: any) {
